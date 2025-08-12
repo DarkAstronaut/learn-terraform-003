c18b6ecc4d505e902283b052a76556b0ce081881
DarkAstronaut <gopalanvenukt@gmail.com>
2025-08-12 02:28:40 -0400

    Commented profile - Need to work on it later

--- providers.tf
+++ providers.tf
@@ -10,11 +10,11 @@ terraform {
 provider "aws" {
   # Default Region and IAM User
   region  = "us-east-2"
-  profile = "Ghost_tf"
+  # profile = "Ghost_tf"
 }
 
 provider "aws" {
   alias   = "west"
   region  = "us-west-1"
-  profile = "Ghost_tf"
+  # profile = "Ghost_tf"
 }
