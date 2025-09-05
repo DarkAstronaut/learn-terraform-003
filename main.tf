3e8dc321802ddc609186772ff45b5df1a618df45
DarkAstronaut <gopalanvenukt@gmail.com>
2025-09-05 11:08:50 -0400

    Final Edits-1

--- main.tf
+++ main.tf
@@ -31,14 +31,14 @@ resource "aws_s3_bucket" "tf-bucket" {
 # Use of Environment Variables
 
 resource "aws_db_instance" "tf-sample-db" {
-  identifier = "sample-db-identifier"
-  allocated_storage = 20
-  storage_type = "gp2"
-  engine = "mysql"
-  engine_version = "5.7"
-  instance_class = "db.t2.micro"
-  username = "admin"
-  password = var.database_pw
+  identifier           = "sample-db-identifier"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = "admin"
+  password             = var.database_pw
   parameter_group_name = "default.mysql5.7"
 }
 
--- variables.tf
+++ variables.tf
@@ -17,51 +17,51 @@ variable "tags" {
 # Example for Sensitive Data
 
 variable "database_pw" {
-  type = string
+  type        = string
   description = "Sensitive Data for Database"
-   # sensitive = true
+  # sensitive = true
 }
 
 # Datatype Examples
 
 # String
 variable "server_name" { # server_name is of String Type
-  type = string
+  type        = string
   description = "Name of the Server"
-  default = "web_server_1"
+  default     = "web_server_1"
 }
 
 # Boolean
 variable "enable_feature" { # enable_feature is Boolean Type
-  type = bool
+  type        = bool
   description = "Enable or Disable Feature"
-  default = true
+  default     = true
 }
 
 # Number
 variable "instance_count" { # instance_count in Number Type, can include Whole Numbers and Decimals
-    type = number
-    description = "Number of Server Instances"
-    default = 3
+  type        = number
+  description = "Number of Server Instances"
+  default     = 3
 }
 
 # List
 variable "my_list" { # my_list is a list of Strings
-  type = list(string)
+  type        = list(string)
   description = "Sample List of String Type"
-  default = [ "val1","val2","3","val4","5" ]
+  default     = ["val1", "val2", "3", "val4", "5"]
 }
 
 # List of any type
 variable "multi_type_list" { # multi_type_list has any primitive type list
-  type = list(any)
+  type        = list(any)
   description = "List of any Type"
-  default = [ "First", 2, "Three", false, "Five", 6.02]
+  default     = ["First", 2, "Three", false, "Five", 6.02]
 }
 
 # Map
 variable "my_map" {
-  type = map(string)
+  type        = map(string)
   description = "Map Representing Key-Value Pair"
   default = {
     key1 = "value1"
@@ -73,22 +73,22 @@ variable "my_map" {
 
 # Set
 variable "my_set" {
-  type = set(string)
-  default = [ "value1", "value2", "value3" ] # Each Value should be unique
-} 
+  type    = set(string)
+  default = ["value1", "value2", "value3"] # Each Value should be unique
+}
 
 # Object
 variable "user" {
-    type = object({
-      name = string
-      email = string
-      age = number
-    }) 
+  type = object({
+    name  = string
+    email = string
+    age   = number
+  })
 }
 // Used for Inputs and Outputs
 
 # Tuple - sequence of values with their own type
 variable "Example_tuple" {
-    type = tuple([ string, number, bool ])
+  type = tuple([string, number, bool])
 }
-// USed for Functions or Modules
+// Used for Functions or Modules
